31033c93c6e0468a7e4bc3e95a639777f13a350e
Gaurav Bisht <gaurav.bisht@tothenew.com>
2024-09-06 18:53:51 +0530

    dev done

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,80 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var showWrongAnswerAlert = false
+    @State private var selectedFlagIndex: Int?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            RadialGradient(stops: [Gradient.Stop(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                                   Gradient.Stop(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
+                                  ],
+                           center: .top,
+                           startRadius: 300,
+                           endRadius: 500)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                VStack(spacing: 30) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            if number == correctAnswer {
+                                score += 1
+                                countries.shuffle()
+                                correctAnswer = Int.random(in: 0...2)
+                            } else {
+                                selectedFlagIndex = number
+                                showWrongAnswerAlert = true
+                            }
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 10)
+                        }
+                    }
+                }
+                .padding(.bottom, 30)
+                .padding(.top, 20)
+                .frame(maxWidth: .infinity)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 20)
+                
+                Spacer()
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .font(.title.bold())
+                    .foregroundColor(.white)
+                
+                Spacer()
+                Spacer()
+            }
+            .padding(.horizontal, 30)
         }
-        .padding()
+        .alert("Wrong!", isPresented: $showWrongAnswerAlert) {
+            Button("Close", role: .cancel) { }
+        } message: {
+            Text(String("You have selected \(countries[selectedFlagIndex ?? 0])'s flag"))
+        }
     }
 }
 
